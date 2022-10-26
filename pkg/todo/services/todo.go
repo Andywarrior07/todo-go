8761e832348ecfdcfd6fa9fcf4f6e3e558a04954
Andres Guerrero C <andresrodrigo.gerrero@externos-cl.cencosud.com>
2022-10-26 20:54:30 -0300

    Add get all todos service and route

--- pkg/todo/services/todo.go
+++ pkg/todo/services/todo.go
@@ -1,10 +1,51 @@
 package services
 
-type Server struct{}
+import (
+	"context"
+	"net/http"
+	"time"
+	"todo-example/pkg/db"
+	"todo-example/pkg/todo/models"
 
-func (s *Server) FindAll() {}
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+)
 
-func (s *Server) FindById() {}
+type Server struct {
+	H db.Handler
+}
+
+func (s *Server) GetAll(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+
+	var todos []models.Todo
+
+	collection := s.H.DB.Database("todos").Collection("todo")
+	result, err := collection.Find(ctx, bson.D{})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+	}
+
+	defer result.Close(ctx)
+
+	for result.Next(ctx) {
+		var todo models.Todo
+
+		if err = result.Decode(&todo); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+		}
+
+		todos = append(todos, todo)
+	}
+
+	c.JSON(http.StatusOK, gin.H{"todos": todos})
+
+}
+
+func (s *Server) GetById() {}
 
 func (s *Server) Create() {}
 
